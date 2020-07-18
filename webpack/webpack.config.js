2580285d9d7de3eca83add8f352a86c14031900a
Mewen Le Roux <mewen.leroux@consulting-for.edenred.com>
2020-07-18 20:05:05 +0200

    Webpack chain convert dev conf

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,31 +1,194 @@
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 const WebpackBar = require('webpackbar')
-const HtmlPlugin = require('html-webpack-plugin')
+const HtmlWebpack = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
 
+// const loaders = require('./helpers/loaders')
+
+// module.exports = {
+
+//   resolve: {
+//     modules: ['node_modules'],
+//     extensions: ['.ts', '.js', '.vue', '.json'],
+//     alias: { '@': path.resolve(__dirname, '..', 'src') },
+//   },
+
+//   module: { rules: loaders },
+
+//   plugins: [
+//     new Dotenv(),
+//     new WebpackBar(),
+//     new VueLoaderPlugin(),
+//     new SpriteLoaderPlugin(),
+//     new HtmlWebpack(htmlOpts),
+//     new CaseSensitivePathsPlugin(),
+//   ],
+
+// }
+
+// Webpack-chain
+// ----------------- //
+/* eslint-disable indent */
+const Config = require('webpack-chain')
+const svgoOptions = require('./optimization/svgo')
+
 const htmlOpts = require('./plugins/html')
-const loaders = require('./helpers/loaders')
 
-module.exports = {
+const config = new Config()
 
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: { '@': path.resolve(__dirname, '..', 'src') },
-  },
 
-  module: { rules: loaders },
+config.resolve
+  .modules
+    .add('node_modules')
+    .end()
+  .alias
+    .set('@', path.resolve(__dirname, '..', 'src'))
+    .end()
+  .extensions
+    .add('.ts')
+    .add('.js')
+    .add('.vue')
+    .add('.json')
+    .end()
 
-  plugins: [
-    new Dotenv(),
-    new WebpackBar(),
-    new VueLoaderPlugin(),
-    new HtmlPlugin(htmlOpts),
-    new SpriteLoaderPlugin(),
-    new CaseSensitivePathsPlugin(),
-  ],
 
-}
+config.module
+  .rule('vue')
+    .test(/\.vue$/)
+    .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+
+config.module
+  .rule('ts')
+    .test(/\.ts?$/)
+    .use('ts-loader')
+      .loader('ts-loader')
+      .options({
+        transpileOnly: true,
+        happyPackMode: false,
+        appendTsSuffixTo: [/\.vue$/],
+      })
+      .end()
+
+config.module
+  .rule('pug')
+    .test(/\.pug?$/)
+    .use('pug-plain-loader')
+      .loader('pug-plain-loader')
+      .end()
+
+config.module
+  .rule('js')
+    .test(/\.js$/)
+    .exclude.add(/(node_modules|bower_components)/)
+      .end()
+    .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+
+config.module
+  .rule('images')
+    .test(/\.(png|jpe?g|gif|ico)$/)
+    .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: 4096,
+        esModule: false,
+        fallback: {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[hash:8].[ext]',
+          },
+        },
+      })
+      .end()
+
+config.module
+  .rule('svg')
+    .test(/\.svg$/)
+    .exclude.add(path.resolve(__dirname, '../..', 'src/assets/icons'))
+      .end()
+    .use('file-loader')
+      .loader('file-loader')
+      .options({
+        loader: 'file-loader',
+        options: {
+          esModule: false,
+          name: 'img/[name].[hash:8].[ext]',
+        },
+      })
+      .end()
+
+config.module
+  .rule('media')
+    .test(/\.(mp4|webm|ogg|mp3|wav|flac|aac)$/)
+    .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: 4096,
+        fallback: {
+          loader: 'file-loader',
+          options: {
+            name: 'media/[name].[hash:8].[ext]',
+          },
+        },
+      })
+      .end()
+
+config.module
+  .rule('font')
+    .test(/\.(woff2?|eot|ttf|otf)$/)
+    .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: 4096,
+        fallback: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[hash:8].[ext]',
+          },
+        },
+      })
+      .end()
+
+config.module
+  .rule('svg-icon')
+    .test(/\.svg$/)
+    .include.add(path.resolve(__dirname, '..', 'src/assets/icons'))
+      .end()
+    .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({ symbolId: 'icon-[name]' })
+      .end()
+    .use('svgo-loader')
+      .loader('svgo-loader')
+      .options(svgoOptions)
+      .end()
+
+
+config
+  .plugin('dotenv')
+    .use(Dotenv)
+    .end()
+  .plugin('webpack-bar')
+    .use(WebpackBar)
+    .end()
+  .plugin('vue-loader-plugin')
+    .use(VueLoaderPlugin)
+    .end()
+  .plugin('sprite-loader')
+    .use(SpriteLoaderPlugin)
+    .end()
+  .plugin('html-webpack')
+    .use(HtmlWebpack, [htmlOpts])
+    .end()
+  .plugin('case-sensitive')
+    .use(CaseSensitivePathsPlugin)
+    .end()
+
+
+module.exports = config
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,87 +1,170 @@
 const path = require('path')
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+// const merge = require('webpack-merge')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
-const baseWebpackConfig = require('./webpack.config')
+// const baseWebpackConfig = require('./webpack.config')
 
-module.exports = merge(baseWebpackConfig, {
+// module.exports = merge(baseWebpackConfig, {
 
-  entry: path.resolve(__dirname, '..', 'server/entry-client.js'),
+//   entry: path.resolve(__dirname, '..', 'server/entry-client.js'),
 
-  output: {
-    filename: 'js/client.bundle.js',
-    chunkFilename: 'js/[name].chunk.js',
-  },
+//   output: {
+//     filename: 'js/client.bundle.js',
+//     chunkFilename: 'js/[name].chunk.js',
+//   },
 
-  devtool: 'inline-source-map',
+//   devtool: 'inline-source-map',
 
-  devServer: {
-    hot: true,
-    quiet: true,
-    host: '0.0.0.0',
-    historyApiFallback: true,
-    watchOptions: {
-      poll: true,
-    },
-  },
+//   devServer: {
+//     hot: true,
+//     quiet: true,
+//     host: '0.0.0.0',
+//     historyApiFallback: true,
+//     watchOptions: {
+//       poll: true,
+//     },
+//   },
 
-  plugins: [
-    new FriendlyErrorsWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+//   plugins: [
+//     new FriendlyErrorsWebpackPlugin(),
+//     new webpack.HotModuleReplacementPlugin(),
+//   ],
 
-  module: {
-    rules: [
-      /* css-loader */
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'vue-style-loader',
-          },
-          {
-            loader: 'css-loader', options: { sourceMap: true },
-          },
-        ],
-      },
+//   module: {
+//     rules: [
+//       /* css-loader */
+//       {
+//         test: /\.css$/,
+//         use: [
+//           {
+//             loader: 'vue-style-loader',
+//           },
+//           {
+//             loader: 'css-loader', options: { sourceMap: true },
+//           },
+//         ],
+//       },
 
-      /* sass-loader */
-      {
-        test: /\.s(a|c)ss$/,
-        use: [
-          {
-            loader: 'vue-style-loader',
-          },
-          {
-            loader: 'css-loader', options: { sourceMap: true },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              config: {
-                path: path.resolve(__dirname, 'helpers/postcss.config.js'),
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sassOptions: { indentedSyntax: true },
-            },
-          },
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              sourceMap: true,
-              resources: path.resolve(__dirname, '..', 'src/assets/styles/**/*.sass'),
-            },
-          },
-        ],
-      },
-    ],
-  },
+//       /* sass-loader */
+//       {
+//         test: /\.s(a|c)ss$/,
+//         use: [
+//           {
+//             loader: 'vue-style-loader',
+//           },
+//           {
+//             loader: 'css-loader', options: { sourceMap: true },
+//           },
+//           {
+//             loader: 'postcss-loader',
+//             options: {
+//               sourceMap: true,
+//               config: {
+//                 path: path.resolve(__dirname, 'helpers/postcss.config.js'),
+//               },
+//             },
+//           },
+//           {
+//             loader: 'sass-loader',
+//             options: {
+//               sourceMap: true,
+//               sassOptions: { indentedSyntax: true },
+//             },
+//           },
+//           {
+//             loader: 'sass-resources-loader',
+//             options: {
+//               sourceMap: true,
+//               resources: path.resolve(__dirname, '..', 'src/assets/styles/**/*.sass'),
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
 
-})
+// })
+
+// Webpack-chain
+// ----------------- //
+/* eslint-disable indent */
+const config = require('./webpack.config.js')
+
+config
+  .entry('index')
+    .add(path.resolve(__dirname, '..', 'server/entry-client.js'))
+    .end()
+  .output
+    .filename('js/client.bundle.js')
+    .chunkFilename('js/[name].chunk.js')
+    .end()
+
+
+config.devtool('inline-source-map')
+
+
+config.devServer
+  .hot(true)
+  .quiet(true)
+  .host('0.0.0.0')
+  .historyApiFallback(true)
+  .watchOptions({ poll: true })
+
+
+config
+  .plugin('friendly-error')
+    .use(FriendlyErrorsWebpackPlugin)
+    .end()
+  .plugin('hmr')
+    .use(webpack.HotModuleReplacementPlugin)
+    .end()
+
+
+config.module
+  .rule('css')
+    .test(/\.css$/)
+    .use('vue-style-loader')
+      .loader('vue-style-loader')
+      .end()
+    .use('css-loader')
+      .loader('css-loader')
+      .options({ sourceMap: true })
+      .end()
+
+config.module
+  .rule('sass')
+    .test(/\.s(a|c)ss$/)
+    .use('vue-style-loader')
+      .loader('vue-style-loader')
+      .end()
+    .use('css-loader')
+      .loader('css-loader')
+      .options({ sourceMap: true })
+      .end()
+    .use('postcss-loader')
+      .loader('postcss-loader')
+      .options({
+        sourceMap: true,
+        config: {
+          path: path.resolve(__dirname, 'helpers/postcss.config.js'),
+        },
+      })
+      .end()
+    .use('sass-loader')
+      .loader('sass-loader')
+      .options({
+        sourceMap: true,
+        sassOptions: { indentedSyntax: true },
+      })
+      .end()
+    .use('sass-resources-loader')
+      .loader('sass-resources-loader')
+      .options({
+        sourceMap: true,
+        resources: path.resolve(__dirname, '..', 'src/assets/styles/**/*.sass'),
+      })
+      .end()
+
+
+module.exports = config.toConfig()
